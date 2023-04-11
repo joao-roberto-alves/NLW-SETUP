542a83ae723687f4bdabde9e3144c38bfd9557e1
João Roberto~ <joaoroberto1406@outlook.com>
2023-04-11 15:51:17 -0300

    add javascript functions

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-  learn: ["01-01"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ⛔")
+    return
+  }
+  //alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
+
+function save() {
+  localStorage.setItem("nlwSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("nlwSetup@habits")) || {}
+
 nlwSetup.setData(data)
 nlwSetup.load()
